a9234dd63e90570eb0ac27e111a830e809f0fa37
jparker2006 <jakeparkeremail@gmail.com>
2022-06-18 00:45:54 -0700

    generate letters based on probabilities (Hawaiian name generator lol)

--- Machine Learning/markov.py
+++ Machine Learning/markov.py
@@ -1,13 +1,15 @@
 import random, sys
 
 def dot(v, m): # vector dot matrice
+    # dot is usually a 3 loop operation.
+    # vector dot matrice means we only need 2 loops.
+    # this is because a vector is a 1 row matrice
     new_vector = []
-    for x in range(1): # 1 would usually be len(m2), since vector, only need 1 iteration
-        for y in range(len(m[0])):
-            current_sum = 0;
-            for z in range(len(v)):
-                current_sum += v[z] * m[z][y];
-            new_vector.append(current_sum);
+    for x in range(len(m[0])):
+        current_sum = 0;
+        for y in range(len(v)):
+            current_sum += v[y] * m[y][x];
+        new_vector.append(current_sum);
     return new_vector
 
 def stochastic_matrix(sFile):
@@ -29,69 +31,41 @@ def stochastic_matrix(sFile):
     return stocm
 
 def main():
-    #sDirectory = sys.argv[1]
-    sDirectory = "/Users/jparker/Desktop/sonnets.txt"
+    sDirectory = sys.argv[1]
     sFile = open(sDirectory, "r+").read()
+    sFile = "".join(sFile.splitlines())
     stocm = stochastic_matrix(sFile)
-
-
     probv = []
     for i in range(256):
         probv.append(0)
     nRand = random.randint(65, 90) # random uppercase letter
     probv[nRand] += 1
-    #sName = chr(nRand)
-    #current_char = sName
-
-    for i in range(100):
-        past_probv = probv
-        probv = dot_product(probv, stocm)
-        if past_probv == probv:
-            #print("stationary state after " + str(i) + " iterations")
-            break
-    print(probv)
-    return
-
-    """
-    for i in range(100):
-        max_value = max(probv[0])
-        max_index = probv[0].index(max_value)
-        if ('\n' == chr(max_index)):
-            print("BREAKING OUT")
-            return
-            probv = [ [] ]
-            for j in range(256):
-                probv[0].append(0)
-            probv[0][random.randint(65, 90)] += 1
-        else:
-            print(current_char)
-            probv[0][ord(current_char)] = 0
-            current_char = chr(max_index)
-            sName += current_char
-            probv[0][ord(current_char)] = 1
-            for j in range(100):
-                past_probv = probv
-                probv = dot_product(probv, stocm)
-                if past_probv == probv:
-                    #print("stationary state after " + str(i) + " iterations")
-                    break
-
-    # put the probv vectors probabilities into the walk function and generate based when you get to the eigenvector
-    probv = [ [] ]
-    for i in range(256):
-        probv[0].append(0)
-
-    probv[0][random.randint(65, 90)] += 1
-
-    for i in range(250):
-        past_probv = probv
-        probv = dot_product(probv, stocm)
-        if past_probv == probv:
-            print("stationary state after " + str(i) + " iterations")
-            break
-
-    """
+    sName = chr(nRand)
+    stoc_index = nRand
+    for i in range(5):
+        for j in range(100):
+            past_probv = probv
+            probv = dot(probv, stocm)
+            if past_probv == probv: # stationary state
+                break
+        aProbs = []
+        for i in range(97, 122):
+            roundedp = round(probv[i], 4) * 100
+            if roundedp < 1: # less than 1% probability
+                continue
+            for j in range(int(round(roundedp))):
+                aProbs.append(chr(i))
+        if 0 == len(aProbs):
+            aProbs.append(chr(random.randint(97, 122)))
+        nNextLetter = random.randint(0, len(aProbs) - 1)
+        cNextLetter = aProbs[nNextLetter]
+        stoc_index = ord(cNextLetter)
+        sName += cNextLetter
+        probv = []
+        for j in range(256):
+            probv.append(0)
+        probv[stoc_index] += 1
+    print(sName)
 
 if "__main__" == __name__:
     main()
-
