b29f66e86bef50f2264fde8c4e6eef3363c0fcc0
Jake Parker <85816763+jparker2006@users.noreply.github.com>
2022-06-22 22:36:40 -0700

    full rewrite to ngrams structure

--- Machine Learning/markov.py
+++ Machine Learning/markov.py
@@ -1,71 +1,44 @@
-import random, sys
+import random, re, sys
 
-def dot(v, m): # vector dot matrice
-    # dot is usually a 3 loop operation.
-    # vector dot matrice means we only need 2 loops.
-    # this is because a vector is a 1 row matrice
-    new_vector = []
-    for x in range(len(m[0])):
-        current_sum = 0;
-        for y in range(len(v)):
-            current_sum += v[y] * m[y][x];
-        new_vector.append(current_sum);
-    return new_vector
-
-def stochastic_matrix(sFile):
-    stocm = []
-    for i in range(256):
-        stocm.append([])
-        for j in range(256):
-            stocm[i].append(0)
-    for i in range(0, len(sFile) - 1):
-        stocm[ord(sFile[i])][ord(sFile[i + 1])] += 1
-    for i in range(256):
-        row_total = 0
-        for j in range(256):
-            row_total += stocm[i][j]
-        if 0 == row_total:
-            continue
-        for j in range(256):
-            stocm[i][j] /= float(row_total)
-    return stocm
+class MarkovChain:
+    def __init__(self, norder, nmax_gen):
+        self.norder = norder
+        self.nmax_gen = nmax_gen
+        self.d_ngrams = {}
+        self.abeginnings = []
+    def feed_chain(self, words):
+        for x in range(len(words)):
+            stext = words[x]
+            for i in range(len(stext) - self.norder):
+                sgram = stext[i:i + self.norder]
+                if 0 == i:
+                    self.abeginnings.append(sgram)
+                cnext = stext[i + self.norder]
+                if sgram not in self.d_ngrams:
+                    self.d_ngrams[sgram] = []
+                self.d_ngrams[sgram].append(cnext)
+    def gen_data(self):
+        scurrent = random.choice(self.abeginnings)
+        sresult = scurrent
+        while len(sresult) < self.nmax_gen:
+            if scurrent in self.d_ngrams:
+                aposs_next = self.d_ngrams[scurrent]
+                snext = random.choice(aposs_next)
+                sresult += snext
+                scurrent = sresult[len(sresult) - self.norder:len(sresult)]
+            else:
+                break
+        return ' '.join(sresult.split('\n'))
 
 def main():
     sDirectory = sys.argv[1]
     sFile = open(sDirectory, "r+").read()
-    sFile = "".join(sFile.splitlines())
-    stocm = stochastic_matrix(sFile)
-    probv = []
-    for i in range(256):
-        probv.append(0)
-    nRand = random.randint(65, 90) # random uppercase letter
-    probv[nRand] += 1
-    sName = chr(nRand)
-    stoc_index = nRand
-    for i in range(5):
-        for j in range(100):
-            past_probv = probv
-            probv = dot(probv, stocm)
-            if past_probv == probv: # stationary state
-                break
-        aProbs = []
-        for i in range(97, 122):
-            roundedp = round(probv[i], 4) * 100
-            if roundedp < 1: # less than 1% probability
-                continue
-            for j in range(int(round(roundedp))):
-                aProbs.append(chr(i))
-        if 0 == len(aProbs):
-            aProbs.append(chr(random.randint(97, 122)))
-        nNextLetter = random.randint(0, len(aProbs) - 1)
-        cNextLetter = aProbs[nNextLetter]
-        stoc_index = ord(cNextLetter)
-        sName += cNextLetter
-        probv = []
-        for j in range(256):
-            probv.append(0)
-        probv[stoc_index] += 1
-    print(sName)
+    delimiters = ['\n', ' ', ',', '.', '?', '!', ':', '_', '-']
+    words = re.split(''.join(delimiters), sFile)
+    chain = MarkovChain(4, 400)
+    chain.feed_chain(words)
+    print('\n' + chain.gen_data() + '\n')
+
 
 if "__main__" == __name__:
     main()
